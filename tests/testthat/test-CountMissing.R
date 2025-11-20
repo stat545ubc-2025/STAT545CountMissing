1be68131da9da199b50cb1a4dafc8fc5058f791a
Nanboy-Ronan <jinruinan@163.com>
2025-11-19 17:40:56 -0800

    add test case

--- tests/testthat/test-CountMissing.R
+++ tests/testthat/test-CountMissing.R
@@ -26,3 +26,30 @@ test_that("Checking error handling for .groups input", {
     count_all_missing_by_group(airquality, Month, NULL)
   )
 })
+
+test_that("Test empty cases correctly", {
+  small_tbl <- tibble(group=character(), var1=numeric())
+
+  expect_equal( small_tbl |> group_by(group) |>
+                  summarize(across(everything(), ~sum(is.na(.x))),
+                            .groups = "drop"),
+                count_all_missing_by_group(small_tbl, group)
+  )
+
+})
+
+test_that("Use keep for .group", {
+  small_tbl <- tribble(~group, ~var1, ~var2,
+                       "A", 1, NA,
+                       "A", 2, "x",
+                       "B", NA, "y",
+                       "C", 3, "z")
+
+  expect_equal( small_tbl |> group_by(group) |>
+                  summarize(across(everything(), ~sum(is.na(.x))),
+                            .groups = "keep"),
+                count_all_missing_by_group(small_tbl, group, "keep")
+  )
+
+})
+
